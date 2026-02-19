050bef49a5ae52cfc88c5c02a2744ef1ffbf09fe
Vinay Sharma <vinay.sharma@wonderbiz.in>
2026-02-19 09:52:48 +0530

    feat: Adding the Project code to the system

--- Backend/GH2-Main/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Backend/GH2-Main/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8903598dffb7206c8e7cadbef0f56d2ff2080357")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
