d2dec19b3dd1e75608384859e2ba816ad8250e32
Vinay Sharma <vinay.sharma@wonderbiz.in>
2026-06-02 12:38:46 +0530

    updating the push

--- Backend/GH2-Main/Gh2-Test/obj/Debug/net9.0/Gh2-Test.AssemblyInfo.cs
+++ Backend/GH2-Main/Gh2-Test/obj/Debug/net9.0/Gh2-Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gh2-Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea67704a77f60fcac8af4b761c33f97eaf8c2cfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c60d8f08cde0ea9f5bc65623e1fe0efe5e6552")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gh2-Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gh2-Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
